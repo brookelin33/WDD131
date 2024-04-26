69fcab0688e55ca24c8d861e75d33e9947098f81
brookelin33 <brookelin33@gmail.com>
2024-04-25 20:24:22 -0600

    mission statemnet css

---
+++ jspractice.js
@@ -0,0 +1,7 @@
+const myApples = 2;
+const friendApples = 4;
+let total = myApples + friendApples;
+
+document.getElementById("myApples").innerHTML = myApples;
+document.getElementById("friendApples").innerHTML = friendApples;
+document.getElementById("total").innerHTML = total;
