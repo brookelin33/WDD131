87bb3dd615aff9b32c76d5b5e93383a0e536a9ac
Brookelin33 <gin23003@byui.edu>
2024-07-19 10:35:07 -0600

    final site complete

--- final/scripts/main.js
+++ final/scripts/main.js
@@ -3,18 +3,21 @@ import { places } from "./places.js";
 places.forEach((_place) => {
   const article = document.createElement("article");
   article.classList.add("place-grid");
-  const htmlelement = "h2";
-  if (_place.name === "Brooke") {
-    htmlelement = "h3";
-  }
-  const h2 = document.createElement(htmlelement);
+  const h2 = document.createElement("h2");
   const img = document.createElement("img");
   img.classList.add("places-img");
   const p = document.createElement("p");
 
   h2.textContent = _place.name;
-  img.setAttribute("src", _place.image);
-  img.setAttribute("alt", _place.name);
+  //conditional branching
+  if (_place.image) {
+    img.setAttribute("src", _place.image);
+    img.setAttribute("alt", _place.name);
+  } else {
+    img.setAttribute("src", "default.jpg");
+    img.setAttribute("alt", "Default Image");
+  }
+
   p.textContent = _place.description;
   article.appendChild(h2);
   article.appendChild(img);
