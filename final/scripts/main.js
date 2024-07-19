06b944c219e9939a26182b69108072571a38245e
Brookelin33 <gin23003@byui.edu>
2024-07-19 10:23:38 -0600

    site plan updated

--- final/scripts/main.js
+++ final/scripts/main.js
@@ -1,27 +1,52 @@
 import { places } from "./places.js";
 
 places.forEach((_place) => {
-    const article = document.createElement("article");
-    article.classList.add("place-grid");
-    const htmlelement = "h2";
-    if (_place.name === "Brooke") {
-        htmlelement = "h3"
-    }
-    const h2 = document.createElement(htmlelement);
-    const img = document.createElement("img");
-    img.classList.add("places-img");
-    const p = document.createElement("p");
+  const article = document.createElement("article");
+  article.classList.add("place-grid");
+  const htmlelement = "h2";
+  if (_place.name === "Brooke") {
+    htmlelement = "h3";
+  }
+  const h2 = document.createElement(htmlelement);
+  const img = document.createElement("img");
+  img.classList.add("places-img");
+  const p = document.createElement("p");
 
-    h2.textContent = _place.name;
-    img.setAttribute("src", _place.image);
-    img.setAttribute("alt", _place.name);
-    p.textContent = _place.description;
-    article.appendChild(h2);
-    article.appendChild(img);
-    article.appendChild(p);
-    document.querySelector("#places").appendChild(article);
+  h2.textContent = _place.name;
+  img.setAttribute("src", _place.image);
+  img.setAttribute("alt", _place.name);
+  p.textContent = _place.description;
+  article.appendChild(h2);
+  article.appendChild(img);
+  article.appendChild(p);
+  document.querySelector("#places").appendChild(article);
 });
 
+document.querySelector("#places").addEventListener("click", viewHandler);
 
-  
+function viewerTemplate(pic, alt) {
+  return `<div class="viewer">
+      <button class="close-viewer">X</button>
+      <img src="${pic}" alt="${alt}">
+      </div>`;
+}
 
+function viewHandler(event) {
+  console.log("view button clicked");
+  // create a variable to hold the element that was clicked on from event.target
+  const clickedElement = event.target;
+  // get the src attribute from that element and 'split' it on the "-"
+  const newSrc = clickedElement.getAttribute("src");
+  document.body.insertAdjacentHTML(
+    "afterbegin",
+    viewerTemplate(newSrc, "alt description")
+  );
+  // add a listener to the close button (X) that calls a function called closeViewer when clicked
+  document
+    .querySelector(".close-viewer")
+    .addEventListener("click", closeViewer);
+}
+
+function closeViewer() {
+  document.querySelector(".viewer").remove();
+}
